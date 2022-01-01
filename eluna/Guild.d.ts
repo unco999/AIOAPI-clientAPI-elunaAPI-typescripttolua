111ecbc18ba108cb08004aaf1d36ed11e54e7588
a19210306 <50286783+unco999@users.noreply.github.com>
2022-01-01 17:21:38 +0800

    Guild 与 item 申明

--- eluna/Guild.d.ts
+++ eluna/Guild.d.ts
@@ -1,3 +1,93 @@
 declare interface Guild{
-    
+    /**
+     * 将指定的玩家添加到指定等级的公会。如果未指定排名，则默认为无。
+     * @param player 要加入公会的玩家
+     * @param rankId 等级 ID。有效数字：从 0 到 255 的整数。
+     */
+    AddMember(player, rankId)
+
+    /**
+     * 从Guild 中移除指定的Player。
+     * @param player 要从公会中移除的玩家。
+     * @param isDisbanding  默认'false'，只有在公会被触发解散时才应该设置为'true'。
+     */
+    DeleteMember(player, isDisbanding)
+
+    /**
+     * 解散公会
+     */
+    Disband()
+
+    /**
+     * 返回公会的条目 ID
+     * 有效数字：从 0 到 4,294,967,295 的整数。
+     */
+    GetId():number
+
+    /**
+     * 返回公会的当前信息
+     */
+    GetInfo():string
+
+    /**
+     * 如果已登录，则通过 GUID查找并返回Guild领导者
+     */
+    GetLeader():Player
+
+    /**
+     * 返回公会领袖 GUID
+     */    
+    GetLeaderGUID():Uint64
+
+    /**
+     * 返回公会的当前消息
+     */    
+    GetMOTD():string
+
+    /**
+     * 返回此公会的成员数
+     */
+    GetMemberCount():number
+
+    /**
+     * 返回一个包含此公会中Player的表只有在线和在某些地图上的玩家。
+     */
+    GetMembers():Object
+
+    /**
+     * 返回公会名称
+     */
+    GetName():string
+
+    /**
+     * 向公会中的所有玩家发送一个WorldPacket
+     */
+    SendPacket(packet:WorldPacket)
+
+    /**
+     * 向公会中指定等级的所有玩家发送WorldPacket
+     * @param packet 数据包worldPacket
+     * @param rankId 等级
+     */
+    SendPacketToRanked(packet:WorldPacket, rankId:number)
+
+    /**
+     * 设置指定银行标签的信息
+     * @param tabId 指定的选项卡的 ID。有效数字：从 0 到 255 的整数。
+     * @param info 要设置到银行选项卡的信息。
+     */
+    SetBankTabText(tabId:number, info:string)
+
+    /**
+     * 设置这个公会的领导者
+     * @param leader 设置的玩家为领导者
+     */
+    SetLeader(leader:Player)
+
+    /**
+     * 
+     * @param player 要降级/升级的玩家
+     * @param rankId 等级 ID 有效数字：从 0 到 255 的整数。
+     */
+    SetMemberRank(player:Player, rankId:number)
 }
--- eluna/item.d.ts
+++ eluna/item.d.ts
@@ -1,5 +1,265 @@
 declare interface Item{
+    /**
+     * 如果项目可以交易，则返回“真”，否则返回“假”
+     */
+    CanBeTraded():boolean
+
+    /**
+     * 从指定槽位的物品上移除附魔
+     * 要移除附魔的插槽 有效数字：从 0 到 4,294,967,295 的整数。
+     */    
+    ClearEnchantment(enchantSlot:number)
+
+    /**
+     * 返回允许使用此项目的Player类
+     */
+    GetAllowableClass():number
+
+    /**
+     * 返回允许使用该物品的玩家种族
+     */    
+    GetAllowableRace():number
+
+    /**
+     * 返回此Item的包大小，如果Item不是包，则返回0
+     */
+    GetBagSize():number
+
+    /**
+     * 返回Item的当前包槽
+     */
+    GetBagSlot():number
+
+    /**
+     * 返回的默认购买数项
+     */
+    GetBuyCount():number
+
+    /**
+     * 返回物品的购买价格
+     */
+    GetBuyPrice():number
+
+    /**
+     * 返回物品的类
+     */
+    GetClass():number
+
+    /**
+     * 返回Item的堆栈计数
+     * 
+     */
+    GetCount():number
+
+    /**
+     * 返回Item的显示 ID
+     */
+    GetDisplayId():number
+
+    /**
+     * 通过指定的附魔槽返回物品的附魔ID
+     */
+    GetEnchantmentId():number
+
+    /**
+     * 返回物品的库存类型
+     */
+    GetInventoryType():number
+
+    /**
+     * 返回Item的级别
+     */
+    GetItemLevel():number
+
+    /**
+     * 返回物品的聊天链接
+     * @param localeConstant 返回Item名称的区域设置。
+     */
+    GetItemLink(localeConstant:LocaleConstant):string
+
+    /**
+     * 返回此的物品组ID项目
+     */
+    GetItemSet():number
+
+    /**
+     * 返回Item的最大堆栈数
+     */
+    GetMaxStackCount():number
+
+    /**
+     * 返回项目的名称
+     */
+    GetName():string
+
+    /**
+     * 返回当前拥有该物品的玩家
+     */
+    GetOwner():Player
+
+    /**
+     * 返回物品的质量
+     */
+    GetQuality():number
+
+    /**
+     * 返回此Item的随机属性 ID
+     */
+    GetRandomProperty():number
+
+    /**
+     * 禁止使用
+     */
+    GetRandomSuffix()
+
+    /**
+     * 返回使用此项目所需的最低级别
+     */
+    GetRequiredLevel():number
+
+    /**
+     * 返回物品的售价
+     */
+    GetSellPrice():number
+
+    /**
+     * 返回Item的当前槽
+     */
+    GetSlot():number
+
+    /**
+     * 通过法术索引返回与物品关联的法术 ID
+     */    
+    GetSpellId():number
+
+    /**
+     * 返回按法术索引绑定到项目的法术触发器
+     * @param spellIndex 指定的拼写索引。
+     * @returns 指定索引的拼写触发器。
+     */
+    GetSpellTrigger(spellIndex:number):number
+
+    /**
+     * 禁止使用
+     */
+    GetStatsCount()
+
+    /**
+     * 返回Item 的子类
+     */
+    GetSubClass():number
+
+    /**
+     * 如果Item指定了与之相关的Quest，则返回“true”，否则返回“ false”
+     * @param questId 要检查的任务ID。
+     */
+    HasQuest(questId:number)
     
+    /**
+     * 如果物品是盔甲牛皮纸，则返回“真”，否则返回“假”
+     */
+    IsArmorVellum():boolean
+
+    /**
+     * 如果Item是包，则返回“true”，否则返回“ false”
+     */
+    IsBag():boolean
+
+    /**
+     * 如果Item是帐户绑定的，则返回“true”，否则返回“ false”
+     */
+    IsBoundAccountWide()
+
+    /**
+     * 如果物品被附魔绑定到玩家，则返回“true”，否则返回“ false”
+     */    
+    IsBoundByEnchant():boolean
+
+    /**
+     * 如果项目损坏，则返回“真” ，否则返回“假”
+     */    
+    IsBroken():boolean
+
+    /**
+     * 如果Item是魔法消耗品，则返回“true”，否则返回“ false”
+     */
+    IsConjuredConsumable():boolean
+
+    /**
+     * 如果Item是货币标记，则返回“true”，否则返回“ false”
+     */
+    IsCurrencyToken():boolean
+
+    /**
+     * 如果该物品当前已装备，则返回“真”，否则返回“假”
+     */
+    IsEquipped():boolean
+
+    /**
+     * 如果Item当前在包中，则返回“true”，否则返回“ false”
+     */
+    IsInBag():boolean
+
+    /**
+     * 如果项目当前在交易窗口中，则返回“真”，否则返回“假”
+     */
+    IsInTrade():boolean
+
+    /**
+     * 如果Item被锁定，则返回“true” ，否则返回“ false”
+     */    
+    IsLocked():boolean
+
+    /**
+     * 如果项目未绑定到指定的player，则返回“真”，否则返回“假”
+     */
+    IsNotBoundToPlayer():boolean
+
+    /**
+     * 如果Item不是空包，则返回“true”，否则返回“ false”
+     */
+    IsNotEmptyBag():boolean
+
+    /**
+     * 如果Item是药水，则返回“true”，否则返回“ false”
+     */
+    IsPotion():boolean
+
+    /**
+     * 如果Item是灵魂绑定的，则返回“true” ，否则返回“ false”
+     */
+    IsSoulBound()
+
+    /**
+     * 如果物品是武器牛皮纸，则返回“真”，否则返回“假”
+     */
+    IsWeaponVellum():boolean
+
+    /**
+     * 将物品保存到数据库
+     */
+    SaveToDB()
+
+    /**
+     * 将Item的绑定设置为“true”或“false”
+     */
+    SetBinding(setBinding:boolean)
+
+    /**
+     * 设置的堆栈数项
+     * @param count 数量
+     */
+    SetCount(count:number)
+
+    /**
+     * 将物品的指定附魔设置到指定槽位
+     */
+    SetEnchantment(enchantId:number, enchantSlot:number)
+
+    /**
+     * 设置指定为项目所有者的玩家
+     */    
+    SetOwner(player:Player)
 }
 
 declare enum EquipmentSlots{
